add29de630cd14ea28f06faff77d07c1ad42b59a
Tomyx K <k_tomyx@freemail.hu>
2013-09-11 21:19:41 +0200

    Be- és kijelentkezés elkészítése

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,14 +11,14 @@ describe "User pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
-	
-	describe "signup page" do
-		before {visit signup_path}
 
-		it{should have_content("Sign up")}
-		it{should have_title(full_title("Sign up"))}
-	end
-	describe "signup" do
+  describe "signup page" do
+    before {visit signup_path}
+
+    it{should have_content("Sign up")}
+    it{should have_title(full_title("Sign up"))}
+  end
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -38,8 +38,20 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
       end
     end
   end
